118f466d7d7d95bfc1b540aa6902002e02a64c0a
LudmilKirov <ludmil9595@gmail.com>
2019-05-27 10:04:28 +0300

    Update DifferentWaysToPrint.java

--- DifferentWaysToPrint.java
+++ DifferentWaysToPrint.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-
+/*Different ways to print on the console*/
 public class DifferentWaysToPrint {
     public static void main(String[] args) {
+
         Scanner scanner=new Scanner(System.in);
+
         String test="print that";
         System.out.println(test);
         System.out.printf("%s\n",test);
         System.out.print(test);
-
-        /*There is */
 
     }
 }

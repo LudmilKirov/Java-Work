1e4a26650203de0d4d815c8aa0b2cde33715269b
LudmilKirov <45374371+LudmilKirov@users.noreply.github.com>
2019-05-29 15:28:43 +0300

    Add files via upload

--- ex.4/Task4.java
+++ ex.4/Task4.java
@@ -6,8 +6,11 @@ public class Task4 {
         double radius = Math.random()*9+1;//Because random gives a double betwen 0-1 multiply it by 9 and add 1
         double height=Math.random()*9+1;
 
-        double capacity =PI*radius*radius*height;
-        double surface=2*PI*radius*height;
+        double roundRadius=Math.round(radius);
+        double roundHeight=Math.round(height);
+
+        double capacity =PI*roundRadius*roundRadius*roundHeight;
+        double surface=2*PI*roundRadius*roundHeight;
 
         System.out.printf("Capacity = %.2f\nSurface = %.2f",capacity,surface);
     }
